5f46ca792614d966d40bdf4c9853f390e947bdac
CodeMacon <147500212+CodeMacon@users.noreply.github.com>
2023-11-27 19:59:17 +0300

    Update SubscribeSingleEvent.java

--- src/main/java/io/xlorey/FluxLoader/annotations/SubscribeSingleEvent.java
+++ src/main/java/io/xlorey/FluxLoader/annotations/SubscribeSingleEvent.java
@@ -4,7 +4,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Subscription to in-game and user events
+ * Subscription to user events
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SubscribeSingleEvent {
