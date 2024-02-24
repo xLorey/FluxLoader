77739b959a0a11199697fb35abaed1fd459f12eb
Deknil <codemacon@gmail.com>
2024-02-24 18:16:08 +0300

    Added empty character constant and methods for colorizing chat messages

--- changelog.md
+++ changelog.md
@@ -1,6 +1,7 @@
 # FluxLoader v0.9.0
 - Added methods for getting and setting lists of specific types for plugin configs
 - Added new methods for configuration files
+- Added empty character constant and methods for colorizing chat messages
 - Changed the logic for saving and loading the standard configuration file
 - Fixed API provisions for chat
 
--- src/main/java/io/xlorey/fluxloader/server/api/ChatUtils.java
+++ src/main/java/io/xlorey/fluxloader/server/api/ChatUtils.java
@@ -1,6 +1,7 @@
 package io.xlorey.fluxloader.server.api;
 
 import lombok.experimental.UtilityClass;
+import zombie.core.Color;
 import zombie.core.raknet.UdpConnection;
 import zombie.network.chat.ChatServer;
 
@@ -13,6 +14,45 @@ import zombie.network.chat.ChatServer;
  */
 @UtilityClass
 public class ChatUtils {
+    /**
+     * White space symbol, used to separate words after applying color, etc. to it.
+     */
+    public static final String SPACE_SYMBOL = "\u200B";
+
+    /**
+     * Getting the color code for coloring a chat message
+     * @param r Red color component in the range 0.0 to 1.0.
+     * @param g The green color component in the range 0.0 to 1.0.
+     * @param b The blue component in the range 0.0 to 1.0.
+     * @return Color code in RGB format.
+     */
+    public static String getColorCode(float r, float g, float b) {
+        return getColorCode(new Color(r, g, b, 1f));
+    }
+
+    /**
+     * Getting the color code for coloring a chat message
+     * @param r Red color component in the range 0 to 255.
+     * @param g The green color component in the range 0 to 255.
+     * @param b The blue component in the range 0 to 255.
+     * @return Color code in RGB format.
+     */
+    public static String getColorCode(int r, int g, int b) {
+        return getColorCode(new Color(r, g, b, 255));
+    }
+
+    /**
+     * Getting the color code for coloring a chat message
+     * @param color Color object.
+     * @return Color code in RGB format.
+     */
+    public static String getColorCode(Color color) {
+        return String.format("<RGB:%.3f,%.3f,%.3f>",
+                color.getR(),
+                color.getG(),
+                color.getB());
+    }
+
     /**
      * Sending a message to a general chat for all users
      * @param text Message text
