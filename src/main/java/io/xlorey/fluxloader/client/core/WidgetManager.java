21921bb4912f04c873c78156dd69373aa5c5a1b6
Deknil <codemacon@gmail.com>
2024-03-07 23:27:22 +0300

    Added `FontAwesome` icons & fonts initialization

--- src/main/java/io/xlorey/fluxloader/client/core/WidgetManager.java
+++ src/main/java/io/xlorey/fluxloader/client/core/WidgetManager.java
@@ -1,15 +1,16 @@
 package io.xlorey.fluxloader.client.core;
 
-import imgui.ImGui;
-import imgui.ImGuiIO;
+import imgui.*;
 import imgui.gl3.ImGuiImplGl3;
 import imgui.glfw.ImGuiImplGlfw;
 import io.xlorey.fluxloader.client.api.TextUtils;
-import io.xlorey.fluxloader.client.ui.pluginMenu.MainMenuButton;
-import io.xlorey.fluxloader.client.ui.pluginMenu.PluginMenu;
+import io.xlorey.fluxloader.client.core.fonts.FontAwesomeIcons;
+import io.xlorey.fluxloader.client.ui.menu.Button;
+import io.xlorey.fluxloader.client.ui.menu.Menu;
 import io.xlorey.fluxloader.client.ui.Widget;
 import io.xlorey.fluxloader.plugin.PluginRegistry;
 import io.xlorey.fluxloader.utils.Constants;
+import io.xlorey.fluxloader.utils.Logger;
 import org.lwjglx.opengl.Display;
 import zombie.GameWindow;
 import zombie.core.Core;
@@ -17,6 +18,11 @@ import zombie.core.opengl.RenderThread;
 import zombie.gameStates.MainScreenState;
 import zombie.ui.UIFont;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -83,6 +89,8 @@ public class WidgetManager {
         // Preventing UI Elements from Saving State
         io.setIniFilename(null);
 
+        initFonts(io);
+
         imGuiGlfw.init(windowHandler, true);
         imGuiGl3.init("#version 330 core");
 
@@ -90,13 +98,52 @@ public class WidgetManager {
     }
 
     /**
+     * Initializing fonts
+     * @param io ImGui IO object
+     */
+    private static void initFonts(ImGuiIO io) {
+        io.getFonts().addFontDefault();
+
+        final ImFontGlyphRangesBuilder rangesBuilder = new ImFontGlyphRangesBuilder(); // Glyphs ranges provide
+        rangesBuilder.addRanges(io.getFonts().getGlyphRangesDefault());
+        rangesBuilder.addRanges(io.getFonts().getGlyphRangesCyrillic());
+        rangesBuilder.addRanges(io.getFonts().getGlyphRangesJapanese());
+        rangesBuilder.addRanges(FontAwesomeIcons._IconRange);
+
+        final ImFontConfig fontConfig = new ImFontConfig();
+        fontConfig.setMergeMode(true);
+
+        final short[] glyphRanges = rangesBuilder.buildRanges();
+        io.getFonts().addFontFromMemoryTTF(loadFromResources("Arial.ttf"), 14, fontConfig, glyphRanges); // cyrillic glyphs
+        io.getFonts().addFontFromMemoryTTF(loadFromResources("NotoSansCJKjp-Medium.otf"), 14, fontConfig, glyphRanges); // japanese glyphs
+        io.getFonts().addFontFromMemoryTTF(loadFromResources("fa-regular-400.ttf"), 14, fontConfig, glyphRanges); // font awesome
+        io.getFonts().addFontFromMemoryTTF(loadFromResources("fa-solid-900.ttf"), 14, fontConfig, glyphRanges); // font awesome
+        io.getFonts().build();
+
+        fontConfig.destroy();
+    }
+
+    /**
+     * Loading fonts from the FluxLoader resource folder
+     * @param name font name
+     * @return byte array of the loaded font
+     */
+    private static byte[] loadFromResources(String name) {
+        try {
+            return Files.readAllBytes(Paths.get(Constants.PATH_FLUXLOADER_RESOURCES, "fonts", name));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
      * Loading a custom plugin management menu
      */
     private static void loadPluginsMenu() {
-        MainMenuButton screenMenuButton = new MainMenuButton();
+        Button screenMenuButton = new Button();
         screenMenuButton.addToScreen();
 
-        PluginMenu pluginMenu = new PluginMenu();
+        Menu pluginMenu = new Menu();
         pluginMenu.addToScreen();
     }
     /**
@@ -108,9 +155,11 @@ public class WidgetManager {
             int margin = 10;
             int screenHeight = Core.getInstance().getScreenHeight();
             int fontHeight = TextUtils.getFontHeight(font);
-            int totalPlugins = PluginRegistry.getLoadedClientPlugins().size() + PluginRegistry.getLoadedServerPlugins().size();
+            int serverCount = PluginRegistry.getLoadedServerPlugins().size();
+            int clientCount = PluginRegistry.getLoadedClientPlugins().size();
+            int totalPlugins = clientCount + serverCount;
             String creditsText = String.format("Patched with %s v%s", Constants.FLUX_NAME, Constants.FLUX_VERSION);
-            String pluginsText = String.format("Loaded plugins: %s", totalPlugins);
+            String pluginsText = String.format("Plugins: %s (Client: %s | Server: %s)", totalPlugins, clientCount, serverCount);
             TextUtils.drawText(creditsText, font, margin,  screenHeight - fontHeight * 2 - margin, 1f, 1f, 1f, 0.5f);
             TextUtils.drawText(pluginsText, font, margin,  screenHeight - fontHeight - margin, 1f, 1f, 1f, 0.5f);
         }
